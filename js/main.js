24bd4b1edd2afec89470d48fda7ef5b7f25df35c
Benjamin L. Havel <Benjamin@bens-mbp-2.home>
2018-11-18 08:04:28 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+var testOne = "What Kind of variable am I";
+
+var testTwo = 256;
+
+var testThree = false;
+
+var testFour = 'true';
+
+var testFive = '128';
+
+var testSix = " ";
+
+var testOne = "hello" + "there";
+
+var testTwo = '100' + 28;
+
+var testThree = true + 3;
+
+var testFour = 'false' + 5;
+
+var testFive = '128' - 3;
+
+var testSix = " " * 9;
+
+var testSeven = 'zero' - 1;
+
+var testEight = 'five'.length;
+
+var testNine = 'five' [0] + 'ever';
+
+var testTen = 'a' > 'b';
+
+var testEleven = 'number' .length < 7;
+
+var testTwelve = '12' == 12;
+
+var testThirteen = 'twelve' == 12;
+
+var testFourteen = '14' === 14;
+
+var testFifteen = 'Hi there' [0] + 'open sesame' [2] + 'alohamora' [1] + 'eulalia' [4] + 'harkonnen' [4] + 'BATMAN!' [6];
+
+
+
+
+
+
+
+
